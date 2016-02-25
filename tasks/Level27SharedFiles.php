e0a1b813a4c28936d7f83a59dc38101be6178d17
Peter FAstré <peter@level27.be>
2016-02-25 22:57:37 +0100

    bugfix and backwards compatibility

--- tasks/Level27SharedFiles.php
+++ tasks/Level27SharedFiles.php
@@ -18,35 +18,33 @@ class Level27SharedFiles extends AbstractTask
         if (strlen($sharedFile) == 0 || $sharedFile == '/')
             return;
 
-        $sharedFolder = $this->getConfig()->deployment('to');
+        $sharedFolder = $this->getConfig()->deployment('to') . '/shared';
         if (strlen($this->getParameter('sharedfolder')))
             $sharedFolder = $this->getParameter('sharedfolder');
 
-        if (strlen($sharedFile) > 0) {
-        //    $sharedFolder = $this->getConfig()->deployment('to') . '/shared';
-            $currentFolder = $this->getConfig()->deployment('to') . '/' . $this->getConfig()->release('directory', 'releases') . '/' . $this->getConfig()->getReleaseId();
+        $currentFolder = $this->getConfig()->deployment('to') . '/' . $this->getConfig()->release('directory', 'releases') . '/' . $this->getConfig()->getReleaseId();
 
-            $file = $sharedFolder . trim($sharedFile);
-            $symlink = $currentFolder . trim($sharedFile);
+        $file = $sharedFolder . trim($sharedFile);
+        $symlink = $currentFolder . trim($sharedFile);
 
-//            if (file_exists($symlink) == true) {
-//                echo "$symlink EXISTS";
-                $renamedsymlink = $symlink . '_RENAMED';
-                if (strpos($symlink, '.') !== false) {
-                    $renamedsymlink = substr($symlink, 0, strpos($symlink, '.')) .
-                        '_RENAMED.' .
-                        substr($symlink, strpos($symlink, '.') + 1);
-                }
-                $command = "mv $symlink $renamedsymlink";
-                $result = $this->runCommandRemote($command);
+        if (file_exists($symlink) == true) {
+            echo "$symlink exists, renaming it";
+            $renamedsymlink = $symlink . '_RENAMED';
+//            if (strpos($symlink, '.') !== false) {
+//                $renamedsymlink = substr($symlink, 0, strpos($symlink, '.')) .
+//                    '_RENAMED.' .
+//                    substr($symlink, strpos($symlink, '.') + 1);
 //            }
-//            else {
-//                echo "$symlink DOES NOT EXIST";
-//            }
-
-            $command = "ln -s $file $symlink";
+            $command = "mv $symlink $renamedsymlink";
             $result = $this->runCommandRemote($command);
         }
+        else {
+           echo "$symlink does not exist, which is ok";
+        }
+
+        $command = "ln -s $file $symlink";
+        $result = $this->runCommandRemote($command);
+
 
         return $result;
     }
